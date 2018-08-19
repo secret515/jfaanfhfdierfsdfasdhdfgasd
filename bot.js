0f53d8aaa9fe2a4bf4fe850e3222f7195925018b
secret515 <41595419+secret515@users.noreply.github.com>
2018-08-20 02:28:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,6 +50,26 @@ const adkar = [
   }
   });
   
+ var prefix = "-اذكار";
+var cats = [
+"https://cdn.discordapp.com/attachments/443574047851413507/480740286629281792/image0.jpg",
+"https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
+"https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg",
+"http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg",
+"https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg",
+"https://vignette1.wikia.nocookie.net/houseofnight/images/8/8b/Cats.jpg/revision/latest?cb=20130812053537",
+"https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg",
+"http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg"
+]
+    client.on('message', message => {
+        var args = message.content.split(" ").slice(1);
+    if(message.content.startsWith(prefix + 'اذكار')) {
+         var cat = new Discord.RichEmbed()
+.setImage(cats[Math.floor(Math.random() * cats.length)])
+message.channel.sendEmbed(cat);
+    }
+});
+
   
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
